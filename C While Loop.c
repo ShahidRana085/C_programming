c21d3b04ef6a3e96a91ef26170213e3f1c361f7d
ShahidRana085 <shahidrana99090@gmail.com>
2025-06-19 02:20:52 -0700

    Update C While Loop.c

--- C While Loop.c
+++ C While Loop.c
@@ -2,44 +2,48 @@
 
 #include <stdio.h>
 
-int main() {
-    int i = 1;
+int main()
+    {
+        int i = 1;
 
-    while (i <= 3) {
-        printf("Count: %d\n", i);
-        i++;
-    }
-
+        while (i <= 3)
+            {
+                printf("Count: %d\n", i);
+                i++;
+            }
     return 0;
-}
+    }
 
 //Code-2: Do/While Loop
 
 #include <stdio.h>
 
-int main() {
-    int i = 1;
+int main()
+    {
+        int i = 1;
 
-    do {
-        printf("Number: %d\n", i);
-        i++;
-    } while (i <= 3);
+        do{
+                printf("Number: %d\n", i);
+                i++;
+            }
+            while (i <= 3);
 
     return 0;
-}
+    }
 
 //Code-3: Real-Life Example
 
 #include <stdio.h>
 
-int main() {
-    int password;
+int main()
+    {
+        int password;
 
-    while (password != 1234) {
-        printf("Enter password: ");
-        scanf("%d", &password);
-    }
-
-    printf("Access granted.");
+        while (password != 1234)
+            {
+                printf("Enter password: ");
+                scanf("%d", &password);
+            }
+            printf("Access granted.");
     return 0;
-}
+    }
