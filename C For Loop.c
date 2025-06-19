4cbcbd560179b065d8db632d333a0e4dc8acc8e6
ShahidRana085 <shahidrana99090@gmail.com>
2025-06-19 02:07:46 -0700

    Update C For Loop.c

--- C For Loop.c
+++ C For Loop.c
@@ -2,39 +2,43 @@
 
 #include <stdio.h>
 
-int main() {
-    for (int i = 1; i <= 3; i++) {
-        printf("Hello %d\n", i);
+int main()
+    {
+        for (int i = 1; i <= 3; i++)
+            {
+                printf("Hello %d\n", i);
+            }
+            return 0;
     }
-
-    return 0;
-}
 
 //Code-2: Nested Loops
 
 #include <stdio.h>
 
-int main() {
-    for (int i = 1; i <= 2; i++) {
-        for (int j = 1; j <= 3; j++) {
-            printf("i = %d, j = %d\n", i, j);
-        }
+int main()
+    {
+        for (int i = 1; i <= 2; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                    {
+                        printf("i = %d, j = %d\n", i, j);
+                    }
+            }
+            return 0;
     }
-
-    return 0;
-}
 
 //Code-3: Real-Life Example
 
 #include <stdio.h>
 
-int main() {
-    for (int i = 1; i <= 5; i++) {
-        printf("Sending message %d\n", i);
+int main()
+    {
+        for (int i = 1; i <= 5; i++)
+            {
+                printf("Sending message %d\n", i);
+            }
+            return 0;
     }
-
-    return 0;
-}
 
 
 
