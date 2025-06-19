ba5e377ecf1de3b2c87329cae00691af936a4aeb
ShahidRana085 <shahidrana99090@gmail.com>
2025-06-19 02:02:16 -0700

    Update C Constants.c

--- C Constants.c
+++ C Constants.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    const int birthYear = 2000; // Constant variable
-    printf("Birth Year: %d\n", birthYear);
+int main()
+    {
+        const int birthYear = 2000; // Constant variable
+        printf("Birth Year: %d\n", birthYear);
 
-    // birthYear = 2001; // This line will give an error if uncommented (cannot change a constant)
+        // birthYear = 2001; // This line will give an error if uncommented (cannot change a constant)
 
-    return 0;
-}
+        return 0;
+    }
