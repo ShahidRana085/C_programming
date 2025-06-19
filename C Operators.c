0adea8d7dc4f97934a1f523604a9a1cbefc26f5a
ShahidRana085 <shahidrana99090@gmail.com>
2025-06-19 02:11:49 -0700

    Update C Operators.c

--- C Operators.c
+++ C Operators.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    int a = 15, b = 4;
+int main()
+    {
+        int a = 15, b = 4;
 
-    // Arithmetic Operators
-    printf("Addition: %d\n", a + b);
-    printf("Subtraction: %d\n", a - b);
-    printf("Multiplication: %d\n", a * b);
-    printf("Division: %d\n", a / b);
-    printf("Modulus: %d\n", a % b);
+        // Arithmetic Operators
+        printf("Addition: %d\n", a + b);
+        printf("Subtraction: %d\n", a - b);
+        printf("Multiplication: %d\n", a * b);
+        printf("Division: %d\n", a / b);
+        printf("Modulus: %d\n", a % b);
 
-    // Relational Operator
-    printf("Is a greater than b? %d\n", a > b); // returns 1 (true)
+        // Relational Operator
+        printf("Is a greater than b? %d\n", a > b); // returns 1 (true)
 
-    // Assignment Operator
-    int c = a;
-    printf("Value of c: %d\n", c);
+        // Assignment Operator
+        int c = a;
+        printf("Value of c: %d\n", c);
 
-    // Logical Operator
-    printf("Logical AND (a > 10 && b < 10): %d\n", (a > 10 && b < 10));
+        // Logical Operator
+        printf("Logical AND (a > 10 && b < 10): %d\n", (a > 10 && b < 10));
 
     return 0;
-}
+    }
