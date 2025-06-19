50d084204d674630d1c339c788c1a18b8eaf6e01
ShahidRana085 <shahidrana99090@gmail.com>
2025-06-19 02:15:25 -0700

    Update C Syntax And Statement.c

--- C Syntax And Statement.c
+++ C Syntax And Statement.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello World!");
-  printf("Have a good day!");// (;) this is statement
+int main()
+  {
+    printf("Hello World!");
+    printf("Have a good day!");// (;) this is statement
   return 0;
-}
+  }
