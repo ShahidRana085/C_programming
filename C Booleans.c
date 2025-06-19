82ab72551cce066b5e43a4b5076a210f22633f76
ShahidRana085 <shahidrana99090@gmail.com>
2025-06-19 01:54:59 -0700

    Update C Booleans.c

--- C Booleans.c
+++ C Booleans.c
@@ -3,25 +3,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
-    bool isRaining = true;
+int main()
+    {
+        bool isRaining = true;
 
-    if (isRaining) {
-        printf("Take an umbrella.");
+        if (isRaining)
+            {
+                printf("Take an umbrella.");
+            }
+            return 0;
     }
-
-    return 0;
-}
 
 //Code-2: Real-Life Example
 
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
-    int age = 20;
-    bool isAdult = (age >= 18);
+int main()
+    {
+        int age = 20;
+        bool isAdult = (age >= 18);
 
-    printf("Is adult: %d", isAdult);
-    return 0;
-}
+        printf("Is adult: %d", isAdult);
+        return 0;
+    }
