77fbefaf01f6d3b5aabc6365320f693b877277fb
ShahidRana085 <shahidrana99090@gmail.com>
2025-06-18 23:55:37 -0700

    Add files via upload

---
+++ C Break_Continue.c
@@ -0,0 +1,78 @@
+//Code:1 Break
+
+#include <stdio.h>
+
+int main()
+	{
+  		int i;
+  
+  		for (i = 0; i < 10; i++)
+        	{
+    			if (i == 4)
+                	{
+      					break;
+    				}
+    				printf("%d\n", i);
+  			}
+   			return 0;
+	}
+
+//Code:2 Continue
+
+#include <stdio.h>
+
+int main()
+	{
+  		int i;
+  
+  		for (i = 0; i < 10; i++)
+        	{
+    			if (i == 4)
+                	{
+      					continue;
+    				}
+    				printf("%d\n", i);
+  			}   
+  			return 0;
+	}
+
+//Break In While Loop
+
+    #include <stdio.h>
+
+int main()
+	{
+  		int i = 0;
+  
+  		while (i < 10)
+        	{
+    			if (i == 4)
+                	{
+      					break;
+    				}
+    				printf("%d\n", i);
+    				i++;
+  			} 
+  			return 0;
+	}
+
+//Continue In While Loop
+
+#include <stdio.h>
+
+int main()
+	{
+  		int i = 0;
+  
+  		while (i < 10)
+        	{
+    			if (i == 4)
+                	{
+      					i++;
+      					continue;
+    				}
+    				printf("%d\n", i);
+    				i++;
+  			} 
+  			return 0;
+	}
